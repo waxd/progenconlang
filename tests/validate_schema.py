8da553c6f2dc31da8841d59a97352665f9d8d663
David Wax <david.allen.wax@gmail.com>
2019-04-23 16:49:15 -0700

    Rewrote the validate xml test to be an actual unit test.

--- tests/validate_schema.py
+++ tests/validate_schema.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
-import os, xmlschema
 
-print('Loading schema')
-schema = xmlschema.XMLSchema('../schemas/conlang.xsd')
-print('Schema Loaded')
-print('Validating xml')
-schema.validate('testdata/basic_cipher.xml')
-print('Valdation complete')
-exit(0)
+import os
+import sys
+import unittest
+import xmlschema
+
+sys.path.append(os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    os.path.pardir,
+    "src"))
+
+from conlang import Conlang
+
+class ValidateXMLTestCase(unittest.TestCase):
+    def test_all_good_examples_load_without_error(self):
+        Conlang.load("testdata/basic_cipher.xml")
+
+    def test_all_bad_examples_throw_error(self):
+        with self.assertRaises(
+                xmlschema.validators.exceptions.XMLSchemaValidationError):
+            Conlang.load("testdata/bad_cipher.xml")
+
+if __name__ == '__main__':
+    unittest.main()
