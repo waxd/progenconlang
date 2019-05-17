08dfb162a4e7fe4060d7d05ac343850265e7a0e4
David Wax <david.allen.wax@gmail.com>
2019-05-16 22:56:19 -0700

    Add a setup file for easier setup in new environments.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='progenconlang',
+    version='0.0',
+    packages=['progenconlang'],
+    url='https://github.com/waxd/progenconlang',
+    license='',
+    author='David Wax',
+    author_email='david.allen.wax@gmail.com',
+    description='',
+    install_requires=[
+        "xmlschema",
+    ],
+)
